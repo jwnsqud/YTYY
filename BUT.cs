0d463f4f667df8a764331c2f92a54224db38e644
강태성 <rkdxotjd93@naver.com>
2018-01-08 16:48:01 +0900

    버튼 대화

---
+++ BUT.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BUT : MonoBehaviour
+{
+    
+    public GameObject B3P;
+    public Text BUT3_1;
+    public Text BUT3_2;
+    public Text BUT3_3;
+    public Text quest;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public void Bu1()
+    {
+        if(txt.BUT1_VALUE=="true")
+        {
+            txt.number += 3;
+          
+            txt.B_number += 7;
+           
+        }
+        else
+        {
+            ++txt.number;
+           
+        }
+        txt.B_T = false;
+    }
+    public void Bu2()
+    {
+        if (txt.BUT2_VALUE == "true")
+        {
+            txt.number += 3;
+            txt.B_number += 7;
+           
+        }
+        else
+        {
+            ++txt.number;
+          
+        }
+        txt.B_T = false;
+
+    }
+
+
+    public void Bu3()
+    {
+        if (txt.BUT3_VALUE == "true")
+        {
+
+            txt.number += 3;
+            txt.B_number += 7;
+          
+        }
+        else
+        {
+            ++txt.number;
+           
+        }
+        txt.B_T = false;
+       
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (txt.B_T == true)
+            
+            B3P.SetActive(true);
+        }
+        else
+            B3P.SetActive(false);
+
+    }
+
+}
---
+++ txt.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class txt : MonoBehaviour
+{
+
+    public GameObject talkP;
+    public GameObject qqqq;
+
+    public static bool T_F = true;
+    public static bool B_T = false;
+    
+    public static int number;
+    public static int B_number;
+    public static int numbersave;
+
+
+    public Text quest;
+    public Text BUT1;
+    public static string BUT1_VALUE;
+    public Text BUT2;
+    public static string BUT2_VALUE;
+    public Text BUT3;
+    public static string BUT3_VALUE;
+
+    string[] all_lines;
+    string[] but_lines;
+    // Use this for initialization
+    void Start()
+    {
+
+        
+        all_lines = File.ReadAllLines(Application.dataPath + "/Resoureces/talkfile.txt");
+        number = 0;
+        but_lines = File.ReadAllLines(Application.dataPath + "/Resoureces/but.txt");
+        B_number = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeSelf == true && B_T == false)
+        {
+            if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.Space))
+            {
+                if (all_lines[number] == ":")
+                {
+                    B_T = true;
+
+                }
+                else if (all_lines[number] == ";")
+                {
+                   number=numbersave;
+                    talkP.SetActive(false);
+                    qqqq.SetActive(true);
+                    T_F = true;
+                }
+                else if (all_lines[number] != "")
+                { quest.text = all_lines[number];
+                    number++;
+                 }
+                
+                
+                else
+                {
+                    talkP.SetActive(false);
+                    number ++;
+                    numbersave = number;
+                }
+               
+                  
+            }
+
+        }
+        if(B_T)
+        {
+            BUT1.text = but_lines[B_number];
+            BUT1_VALUE = but_lines[B_number + 1];
+
+            BUT2.text = but_lines[B_number + 2];
+            BUT2_VALUE = but_lines[B_number + 3];
+
+            BUT3.text = but_lines[B_number +4];
+            BUT3_VALUE = but_lines[B_number + 5];
+
+
+        }
+
+    }
+}
