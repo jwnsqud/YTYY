671d518f93187ad946a8d8855b31c77c290cbb06
SE DEUN LEE <31530230+SEDEUN@users.noreply.github.com>
2018-01-08 11:53:14 +0900

    클릭 가능한 아이템에 다가갈 때 아이템에서 하이라이트 효과 발생, 멀어지면 하이라이트 효과 제거

---
+++ object highlight.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clickItem : MonoBehaviour
+{
+	public GameObject deletegameObject;
+	public GameObject Particle;
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+	}
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.CompareTag("Player"))
+		{
+			Debug.Log("들어감");
+			Particle.SetActive(true);
+		}
+	}
+
+	private void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.CompareTag("Player"))
+		{
+			Particle.SetActive(true);
+		}
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Destroy(deletegameObject);
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.CompareTag("Player"))
+		{
+			Particle.SetActive(false);
+		}
+	}
+
+}
