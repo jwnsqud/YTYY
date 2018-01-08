0d7d959c54b15fd9b02422c9e19201747cae614c
dusdhks7 <31647733+dusdhks7@users.noreply.github.com>
2018-01-08 11:46:25 +0900

    아이템 주울시 Announce와 panal을 작동시킨다. 오브젝트에 적용할것.

---
+++ Announce.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Announce : MonoBehaviour {
+    public static bool inform = false;
+    
+
+    public Text Alram;
+
+	void Start () {
+
+        //Alram = GetComponent<Text>();
+        Alram.text = "획득한 증거품 : ";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+        {
+        
+        if (inform == true)
+        {
+            Alram.text = Route.target1 + "을 획득했다.";
+           
+        }
+        
+    }
+
+    
+
+}
+
---
+++ panal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class panal : MonoBehaviour
+{
+    private int done;
+    private float reset;
+    public GameObject Talkbox;
+
+    private void Start()
+    {
+        done = 2;
+        reset = 0.0f;
+        Talkbox.SetActive(false);
+    }
+    private void Update()
+    {
+        if (Announce.inform == true)
+        {
+            reset += Time.deltaTime;
+            Talkbox.SetActive(true);
+            if(reset>done)
+            {
+                Talkbox.SetActive(false);
+                Announce.inform = false;
+                reset = 0;
+            }
+
+        }
+
+    }
+}
