69e2e4ffdb0d7f70e85eafb184a07a6ad76e2e49
dusdhks7 <31647733+dusdhks7@users.noreply.github.com>
2018-01-08 16:45:00 +0900

    씬마다 오브젝트 카운터를 변경 route와 연계하고 empty파일을 만들어 넣는다. eventmanager개념,

---
+++ opendoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opendoor : MonoBehaviour {
+
+    public static int Object_num;
+    public static int Count_num;
+
+	// Use this for initialization
+	void Start () {
+        Object_num = 3;//숫자에 씬의 오브젝트 숫자 적용
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Count_num == Object_num)
+        {
+            Debug.Log("ddfas");//문여는 메소드 트루값으루라다 넣어버리기~
+        }
+		
+	}
+
+    public void counter()
+    {
+        Count_num = 0;
+    }
+}
---
+++ playermove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour {
+
+    Animator Animator;
+    public float speed = 1.5f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            GetComponent<Animator>().SetTrigger("Right");
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            GetComponent<Animator>().SetTrigger("Left");
+        }
+        }
+	}
+
